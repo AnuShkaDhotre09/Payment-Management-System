cd5f0c7d0622a9629100585c3fb73755bacb1152
Anushka Ashok Dhotre <101286486+AnuShkaDhotre09@users.noreply.github.com>
2024-03-18 20:16:58 +0530

    Add files via upload

---
+++ src/app/shared/payment-detail.ts
@@ -0,0 +1,7 @@
+export class PaymentDetail {
+    id:number=0
+    name:string=""
+    cardNumber:string=""
+    expirationDate:string=""
+    securityCode:string=""
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiBaseUrl:'http://localhost:46032/api'
+};
